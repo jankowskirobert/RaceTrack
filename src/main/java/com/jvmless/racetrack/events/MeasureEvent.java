e475eed5250fe6e142c71b7026a6643c24a36195
Robert Jankowski <jankowski.r@hotmail.com>
2018-09-12 22:54:26 +0200

    Add event order test, refactor event

--- src/main/java/com/jvmless/racetrack/events/MeasureEvent.java
+++ src/main/java/com/jvmless/racetrack/events/MeasureEvent.java
@@ -2,14 +2,21 @@ package com.jvmless.racetrack.events;
 
 import com.jvmless.racetrack.Checkpoint;
 import com.jvmless.racetrack.CompetitorNumber;
+import com.jvmless.racetrack.TrackEvent;
+import com.jvmless.racetrack.TrackSession;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor(staticName = "of")
-public class MessureEvent {
+public class MeasureEvent implements TrackEvent {
     private LocalDateTime occurrence;
     private Checkpoint checkpoint;
     private CompetitorNumber number;
+
+    @Override
+    public TrackSession getTrackSession() {
+        return null;
+    }
 }
